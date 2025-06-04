589dbf8218017102de43603af086285d0143b457
Shivansh <shivansh2162001@gmail.com>
2025-06-04 13:13:51 -0600

    retrieved the data from api for dynamic quotes on click

--- quotes-app/src/components/Quotes-App.jsx
+++ quotes-app/src/components/Quotes-App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react"
 const QuotesApp = () => {
+    const [quote, setQuote] = useState({text: "Soon 150k", author: "Shivansh"})
+
+    const fetchNewQuote = async () => {
+        const url = 'http://api.quotable.io/random'
+        const response = await fetch(url)
+        if (!response.ok) throw new Error('Failed to fetch');
+        const responseData = await response.json()
+        console.log(responseData)
+        setQuote({
+            text: responseData.content,
+            author: responseData.author
+        })
+    }
+
     return <div className="container">
         <div className="quotes-app">
             <h1 className="app-heading">Quotes.</h1>
             <i className="bx bxs-heart fav-icon"></i>
             <div className="quote">
                 <i className="bx bxs-quote-alt-left left-quote"></i>
-                <p className="quote-text">Soon 150k</p>
-                <p className="quote-author">Shivansh</p>
+                <p className="quote-text">{quote.text}</p>
+                <p className="quote-author">{quote.author}</p>
                 <i className="bx bxs-quote-alt-right right-quote"></i>
             </div>
             <div className="circles">
@@ -17,7 +32,7 @@ const QuotesApp = () => {
             </div>
 
             <div className="buttons">
-                <button className="btn btn-new-quote">New Quote</button>
+                <button className="btn btn-new-quote" onClick={fetchNewQuote}>New Quote</button>
                 <button className="btn btn-favorite">Add to Favorites</button>
             </div>
         </div>
