fb419432fb531f2e2bf66095209550066d585154
Shivansh <shivansh2162001@gmail.com>
2025-06-04 16:14:25 -0600

    Favorites section functional with add, delete and duplicate handling

--- quotes-app/src/components/Quotes-App.jsx
+++ quotes-app/src/components/Quotes-App.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react"
 const QuotesApp = () => {
     const [quote, setQuote] = useState({text: "Soon 150k", author: "Shivansh"})
+    const [showFavSection, setShowFavSection] = useState(false)
+    const [favorites, setFavorites] = useState([])
 
     const fetchNewQuote = async () => {
         const url = 'http://api.quotable.io/random'
         const response = await fetch(url)
         if (!response.ok) throw new Error('Failed to fetch');
         const responseData = await response.json()
-        console.log(responseData)
         setQuote({
             text: responseData.content,
             author: responseData.author
         })
     }
 
+    const addToFavorites = () => {
+        if (!favorites.some((fav) => fav.text === quote.text && fav.author === quote.author)) {
+            setFavorites([...favorites, quote])
+        }
+    }
+
     return <div className="container">
         <div className="quotes-app">
             <h1 className="app-heading">Quotes.</h1>
-            <i className="bx bxs-heart fav-icon"></i>
+            <i className="bx bxs-heart fav-icon" onClick = {()=>{setShowFavSection(prev => !prev)}}></i>
             <div className="quote">
                 <i className="bx bxs-quote-alt-left left-quote"></i>
                 <p className="quote-text">{quote.text}</p>
@@ -33,20 +40,26 @@ const QuotesApp = () => {
 
             <div className="buttons">
                 <button className="btn btn-new-quote" onClick={fetchNewQuote}>New Quote</button>
-                <button className="btn btn-favorite">Add to Favorites</button>
+                <button className="btn btn-favorite" onClick = {addToFavorites}>Add to Favorites</button>
             </div>
-            <div className="favorites">
+           {showFavSection && (<div className="favorites">
                 <button className="btn-close">
-                    <i className="bx bx-x"></i>
+                    <i className="bx bx-x" onClick = {() => setShowFavSection(false)}></i>
                 </button>
-                <div className="fav-quote">
-                    <div className="fav-quote-delete">
-                        <i className="bx bx-x-circle"></i>
+                {favorites.map((favQuote, index) =>(
+                    <div className="fav-quote" key={index}>
+                        <div className="fav-quote-delete">
+                            <i className="bx bx-x-circle"
+                            onClick={()=>{
+                                const updatedFavorites = favorites.filter((item, i) => i !== index)
+                                setFavorites(updatedFavorites)
+                            }}></i>
+                        </div>
+                        <div className="fav-quote-text">{favQuote.text}</div>
+                        <div className="fav-quote-author">{favQuote.author}</div>
                     </div>
-                    <div className="fav-quote-text">HI! I am in the favorite</div>
-                    <div className="fav-quote-author">Shivansh</div>
-                </div>
-            </div>
+            ))}
+            </div>)}
         </div>
     </div>
 }
